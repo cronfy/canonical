799fe131fc8e6822a7c17b3e2afa4f127a5c42c9
Cronfy <cronfy@gmail.com>
2017-03-17 19:23:32 +0300

    initial

---
+++ src/Request.php
@@ -0,0 +1,121 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: cronfy
+ * Date: 11.03.17
+ * Time: 13:20
+ */
+
+namespace cronfy\canonical;
+use Yii;
+
+class Request
+{
+    protected
+        $canonical_scheme,
+        $canonical_host
+    ;
+
+    public function getHost() {
+        if (!$host = @$_SERVER['HTTP_HOST'] ?: @$_SERVER['SERVER_NAME']) {
+            throw new \Exception("Failed to detect host");
+        }
+
+        return $host;
+    }
+
+    public function getCanonicalHost() {
+        return $this->canonical_host ?: $this->getHost();
+    }
+
+    /**
+     * @param string $canonical_host
+     */
+    public function setCanonicalHost($canonical_host)
+    {
+        $this->canonical_host = $canonical_host;
+    }
+
+    /**
+     * @return \yii\web\Request
+     */
+    public function getRequest() {
+        if (Yii::$app) {
+            return Yii::$app->request;
+        }
+
+        return new \yii\web\Request();
+    }
+
+    public function warnEscapeFromHttps() {
+        // необходимо в <head> указать следующий тег:
+        //
+        // <meta name="referrer" content="origin-when-cross-origin"/>
+        //
+        // Он означает, что при переходе по ссылке с https на http БУДЕТ
+        // передаваться referrer в виде schema://host(:port)/
+        // Если этот тег не указать, то при переходе на http не будет
+        // передаваться referrer, и мы не узнаем, с какой страницы произошел такой переход.
+
+        $request = $this->getRequest();
+        $canonical_host = $this->getCanonicalHost();
+
+        if ($referrer = $request->referrer) {
+            if (
+                // referrer - SSL на нашем сайте
+                preg_match('#https://' . $canonical_host. '#', $referrer)
+                // а сейчас не SSL
+                && !$request->isSecureConnection
+            ) {
+                Yii::warning(
+                    'Connection was forwarded to not secure: '
+                    . $referrer
+                    . ' => '
+                    . $request->hostInfo . $_SERVER['REQUEST_URI']
+                    ,
+                    'app/ssl'
+                );
+            }
+        }
+    }
+
+    public function getScheme() {
+        $request = $this->getRequest();
+        return $request->isSecureConnection ? 'https' : 'http';
+    }
+
+    /**
+     * @return string
+     */
+    public function getCanonicalScheme()
+    {
+        return $this->canonical_scheme ?: $this->getScheme();
+    }
+
+    /**
+     * @param string $canonical_scheme
+     */
+    public function setCanonicalScheme($canonical_scheme)
+    {
+        $this->canonical_scheme = $canonical_scheme;
+    }
+
+    public function redirectToCanonical() {
+        $request = $this->getRequest();
+
+        $canonical_scheme = $this->getCanonicalScheme();
+        $canonical_host = $this->getCanonicalHost();
+
+        // redirect to https
+        if ($request->isGet) {
+            if ($this->getScheme() != $canonical_scheme || $this->getHost() != $canonical_host) {
+                $url = $canonical_scheme . '://' . $canonical_host . $_SERVER['REQUEST_URI'];
+                header("HTTP/1.1 301 Moved Permanently");
+                header("Location: $url");
+                exit();
+            }
+        }
+    }
+
+
+}
